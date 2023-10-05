ced6cd8a888b245475e9818825c78aab5687445a
Valentin Pundikov <74809607+ValentinPundikov@users.noreply.github.com>
2023-10-05 17:40:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ logging.basicConfig(level=logging.INFO)
 dp.callback_query.register(get_weather_today_coordinates,F.data=="data_today")
 dp.callback_query.register(get_weather_five_days_coordinates,F.data=="data_five_days")
 dp.callback_query.register(back,F.data=="back")
-dp.message.register(get_weather, F.text == "Получить погоду на местности")
+dp.message.register(get_weather, F.location)
 dp.message.register(get_weather_Moscow, F.text == "Получить погоду в Москве")
-dp.message.register(get_weather_Tambov(), F.text == "Получить погоду в Тамбове")
+dp.message.register(get_weather_Tambov, F.text == "Получить погоду в Тамбове")
 dp.message.register(start, F.text == "/start")
 
 async def main():
